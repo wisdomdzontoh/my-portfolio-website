a9df2f391e6741cdd6de6d81ae10809c3257db9e
wisdomdzontoh <wisdomdzontoh@gmail>
2024-03-01 12:30:46 +0000

    update about errors

--- src/app/components/AboutSection.jsx
+++ src/app/components/AboutSection.jsx
@@ -18,7 +18,7 @@ const TAB_DATA = [
         <li>Java</li>
         <li>Python</li>
         <li>PHP</li>
-        <li>API's</li>
+        <li>APIs</li>
         <li>Flutter</li>
       </ul>
     ),
@@ -59,39 +59,27 @@ const AboutSection = () => {
   return (
     <section className="text-white" id="about">
       <div className="md:grid md:grid-cols-2 gap-8 items-center py-8 px-4 xl:gap-16 sm:py-16 xl:px-16">
-        <Image src="/images/about-image.png" width={500} height={500} />
+        <Image src="/images/about-image.png" width={500} height={500} alt="About Me Image" />
         <div className="mt-4 md:mt-0 text-left flex flex-col h-full">
           <h2 className="text-4xl font-bold text-white mb-4">About Me</h2>
           <p className="text-base lg:text-lg">
-            I am a full stack web developer and experience health data analyst with a passion for creating
+            I am a full stack web developer and experienced health data analyst with a passion for creating
             interactive and responsive web applications. I have experience
-            working with JavaScript, React, php, Node.js, Express, PostgreSQL,
-            MySql,mongoDb,RESTful API's, HTML, CSS, and Git. I am a quick learner and I am always
+            working with JavaScript, React, PHP, Node.js, Express, PostgreSQL,
+            MySQL, MongoDB, RESTful APIs, HTML, CSS, and Git. I am a quick learner and I am always
             looking to expand my knowledge and skill set. I am a team player and
             I am excited to work with others to create amazing applications and data science projects.
           </p>
           <div className="flex flex-row justify-start mt-8">
-            <TabButton
-              selectTab={() => handleTabChange("skills")}
-              active={tab === "skills"}
-            >
-              {" "}
-              Skills{" "}
-            </TabButton>
-            <TabButton
-              selectTab={() => handleTabChange("education")}
-              active={tab === "education"}
-            >
-              {" "}
-              Education{" "}
-            </TabButton>
-            <TabButton
-              selectTab={() => handleTabChange("certifications")}
-              active={tab === "certifications"}
-            >
-              {" "}
-              Certifications{" "}
-            </TabButton>
+            {TAB_DATA.map((tabItem) => (
+              <TabButton
+                key={tabItem.id}
+                selectTab={() => handleTabChange(tabItem.id)}
+                active={tab === tabItem.id}
+              >
+                {tabItem.title}
+              </TabButton>
+            ))}
           </div>
           <div className="mt-8">
             {TAB_DATA.find((t) => t.id === tab).content}
